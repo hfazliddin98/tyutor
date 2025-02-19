409223d12528fe428ba1317679fad7736e826518
hfazliddin98 <hatamovfazliddin505@gmail.com>
2025-02-19 14:30:15 +0500

    19.2.2025

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, FakultetViewSet, YonalishViewSet, KursViewSet, Guruh
+from .views import UserViewSet, FakultetViewSet, YonalishViewSet, KursViewSet, GuruhViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 router.register(r'fakultet', FakultetViewSet, basename='fakultet')
 router.register(r'yonalish', YonalishViewSet, basename='yonalish')
 router.register(r'kurs', KursViewSet, basename='kurs')
-router.register(r'guruh', Guruh, basename='guruh')
+router.register(r'guruh', GuruhViewSet, basename='guruh')
 
 
 urlpatterns = []
